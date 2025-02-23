7be43b67579c420d48dc1e87292258c97a6236cd
Prachiti <prachitiagarkar1@gmail.com>
2025-02-23 15:01:48 +0530

    [Prachiti Agarkar] add: Give greeting mesg for different conditions

---
+++ UC3GreetingService/UC3GreetingService/src/main/java/com/example/UC3GreetingService/Uc3GreetingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.UC3GreetingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc3GreetingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc3GreetingServiceApplication.class, args);
+	}
+
+}
