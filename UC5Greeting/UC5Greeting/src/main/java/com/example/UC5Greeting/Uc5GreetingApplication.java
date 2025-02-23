05ca88006622ab7e82566ae49cddf574249bb42e
Prachiti <prachitiagarkar1@gmail.com>
2025-02-23 15:04:38 +0530

    [Prachiti Agarkar] add: find greeting mesg by id in the repository

---
+++ UC5Greeting/UC5Greeting/src/main/java/com/example/UC5Greeting/Uc5GreetingApplication.java
@@ -0,0 +1,13 @@
+package com.example.UC5Greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc5GreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc5GreetingApplication.class, args);
+	}
+
+}
